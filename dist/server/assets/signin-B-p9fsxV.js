5725e69f84055d07b5235b78173a26d69c2a1c89
adajoy <adajoyyy@hotmail.com>
2025-12-24 11:41:04 +0800

    Update package dependencies, add preview script, and remove obsolete client/server assets. Bump versions for several packages including React, Prisma, and Tailwind CSS for improved performance and security.

--- dist/server/assets/signin-B-p9fsxV.js
+++
@@ -1,5 +0,0 @@
-import { jsx } from 'react/jsx-runtime';
-function RouteComponent() {
-	return /* @__PURE__ */ jsx('div', { children: 'Hello "/signin"!' });
-}
-export { RouteComponent as component };
